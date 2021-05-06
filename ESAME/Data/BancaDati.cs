520246f765a6186de92732e7327ef35eee3579f7
madCat00 <francescoxx35@gmail.com>
2021-05-06 13:17:13 +0200

    Aggiungere i file di progetto.

---
+++ ESAME/Data/BancaDati.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESAME.Data
+{
+    /// <summary>
+    /// Si eredita dalla classe Dbcontext, quello che serve per poter effettuare la connessione al database
+    /// </summary>
+    public class BancaDati : DbContext
+
+    {
+        /// <summary>
+        /// Si indicano quale è la classe per interfacciarsi con il db,(ovvero piatto in questo caso), e la tabella del database
+        /// </summary>
+        public DbSet<Piatto> piatti { get; set; }
+
+        /// <summary>
+        /// Infine si procede con la predisposizione della connessione vera e propria al nostro database
+        /// </summary>
+        /// <param name="options"></param>
+        public BancaDati(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ ESAME/Data/Piatto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESAME.Data
+{
+    public class Piatto
+    {
+        /// <summary>
+        /// Si Inserisci l'id come campo per poter accedere al dato id in banca dati
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// Si inserisce il campo portata di tipo string per accedere al dato portata in banca dati
+        /// </summary>
+        
+        [Required(ErrorMessage ="Devi inserire la portata!!")]
+        public string portata { get; set; }
+
+        /// <summary>
+        /// Si inserisce il campo portata di tipo string per accedere al dato allergeni in banca dati
+        /// </summary>
+        /// 
+
+        [Required(ErrorMessage = "Devi inserire gli allergeni!!")]
+        public string allergeni { get; set; }
+
+        /// <summary>
+        /// Si inserisce il campo portata di tipo string per accedere al dato stagione in banca dati
+        /// </summary>
+
+        [Required(ErrorMessage = "Devi inserire la stagione!!")]
+        public string stagione { get; set; }
+
+        /// <summary>
+        /// Si inserisce il campo portata di tipo string per accedere al dato prezzo in banca dati
+        /// </summary>
+
+        [Required(ErrorMessage = "Devi inserire il prezzo!!")]
+        public float prezzo { get; set; }
+
+        /// <summary>
+        /// Si inserisce un ulteriore campo oltre a quello richiesto, per poter far esprimere al cliente anche un giudizio sul piatto
+        /// </summary>
+        public string note { get; set; }
+    }
+}
